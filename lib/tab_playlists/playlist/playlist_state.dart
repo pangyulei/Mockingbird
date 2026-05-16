6ce0928088f39272b112cbeca3d4b0ab86e35ca8
ray <pangyulei@gmail.com>
2026-05-17 06:21:58 +0800

    Fix deppcopy

--- lib/tab_playlists/playlist/playlist_state.dart
+++ lib/tab_playlists/playlist/playlist_state.dart
@@ -4,4 +4,11 @@ class PlaylistState {
   final Playlist? playlist;
   final bool showLoading;
   const PlaylistState({this.playlist, this.showLoading = true});
+
+  PlaylistState copyWith({Playlist? playlist, bool? showLoading}) {
+    return PlaylistState(
+      playlist: (playlist ?? this.playlist)?.copyWith(),
+      showLoading: showLoading ?? this.showLoading,
+    );
+  }
 }
