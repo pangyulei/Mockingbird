051f2fa4ec2a4f8721f60bf1e62914d8b1a2c206
ray <pangyulei@gmail.com>
2026-09-10 07:52:17 +0800

    Refresh albumdetail while back to foreground

--- lib/tab_albums/album_detail/album_detail_bloc.dart
+++ lib/tab_albums/album_detail/album_detail_bloc.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:defer/defer.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:mockingbird/tab_albums/album_detail/album_detail_event.dart';
@@ -8,50 +9,52 @@ import 'package:mockingbird/tab_albums/album_detail/album_detail_ui.dart';
 import 'package:mockingbird/tab_albums/media_card/media_card_bloc.dart';
 import 'package:mockingbird/tab_albums/media_card/media_card_event.dart';
 import 'package:mockingbird/tab_albums/media_card/media_card_ui.dart';
+import 'package:mockingbird/tool/event_hub.dart';
 import 'package:mockingbird/tool/extensions.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 class AlbumDetailBloc extends AlbumDetailBlocType {
   final String? _albumId;
+  final _subscriptionList = <StreamSubscription>[];
+
   AlbumDetailBloc(this._albumId) : super(const AlbumDetailInitState()) {
     on<AlbumDetailInitEvent>(_onInit);
+    _subscriptionList.addAll([EventHub.on<HubAppResumeEvent>(_onAppResume)]);
   }
 
   @override
   Future<void> close() {
-    debugPrint('albumdetail close');
+    for (final sub in _subscriptionList) {
+      sub.cancel();
+    }
     return super.close();
   }
 
-  void _onInit(
-    AlbumDetailInitEvent event,
-    Emitter<AlbumDetailState> emit,
-  ) async {
+  void _onInit(AlbumDetailInitEvent event, Emitter<AlbumDetailState> emit) async {
     EasyLoading.show(maskType: .clear);
-    await defer(
-      () async {
-        EasyLoading.dismiss();
-      },
-      () async {
-        if (_albumId == null) {
-          emit(const AlbumDetailNotFoundState());
-          return;
-        }
-        final album = await AssetPathEntity.fromId(_albumId);
-        final mediaList = await album.getAssetListRange(
-          start: 0,
-          end: await album.assetCountAsync,
-        );
-        // Sort by title ascending (case-insensitive)
-        mediaList.sort((a, b) {
-          final aTitle = a.title?.toLowerCase() ?? '';
-          final bTitle = b.title?.toLowerCase() ?? '';
-          return aTitle.compareTo(bTitle);
-        });
-        emit(AlbumDetailDataState(name: album.name, mediaList: mediaList));
-        emit(AlbumDetailDataState(name: album.name, mediaList: mediaList));
-      },
-    );
+    emit(await _reload());
+    EasyLoading.dismiss();
+  }
+
+  void _onAppResume(HubAppResumeEvent event) async {
+    EasyLoading.show(maskType: .clear);
+    emit(await _reload());
+    EasyLoading.dismiss();
+  }
+
+  Future<AlbumDetailState> _reload() async {
+    if (_albumId == null) {
+      return const AlbumDetailNotFoundState();
+    }
+    final album = await AssetPathEntity.fromId(_albumId);
+    final mediaList = await album.getAssetListRange(start: 0, end: await album.assetCountAsync);
+    // Sort by title ascending (case-insensitive)
+    mediaList.sort((a, b) {
+      final aTitle = a.title?.toLowerCase() ?? '';
+      final bTitle = b.title?.toLowerCase() ?? '';
+      return aTitle.compareTo(bTitle);
+    });
+    return AlbumDetailDataState(name: album.name, mediaList: mediaList);
   }
 
   @override
